43c8699cf0e28c0824e8bbaa085aa2d6cb8de604
Manjyyot <manjyotsinghchaudhary@gmail.com>
2024-09-26 20:40:53 +0530

    Add script to create directory structure

---
+++ create_directory_structure.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create project directories
+mkdir -p ~/projects/project1
+mkdir -p ~/projects/project2
+mkdir -p ~/projects/project3
+
+# Confirm that the directory structure has been created
+echo "Project directories created."
